e4152600f3f1cfd2bebb20acac9c17402d3c8142
Joshua Davis <jad041@uark.edu>
2022-03-18 15:50:53 -0500

    Update chair.py
    Simple starter code for analog readings

--- package_name/chair.py
+++ package_name/chair.py
@@ -1,12 +1,41 @@
-from pyfirmata import Arduino, util
+import pyfirmata
 import time
-board = Arduino("COM8")
 
-loopTimes = input ('Input number of blinks: ')
-print("Blinking " + loopTimes + " times.")
+# Variables ###############################
+board = pyfirmata.Arduino("COM8")
 
-for i in range (int(loopTimes)):
+# Setup ###################################
+a0 = board.get_pin('a:0:i')
+it = pyfirmata.util.Iterator(board) 
+it.start()
+
+# Loop ####################################
+while True:
+    #Update Analog
+    print(a0.read())
+    #Blink the on-board LED
     board.digital[13].write(1)
-    time.sleep(0.2)
+    time.sleep(0.5)
     board.digital[13].write(0)
-    time.sleep(0.2)
+    time.sleep(0.5)
+
+
+
+
+
+
+# loopTimes = input ('Input number of blinks: ')
+# print("Blinking " + loopTimes + " times.")
+
+# for i in range (int(loopTimes)):
+#     board.digital[13].write(1)
+#     time.sleep(1)
+#     board.digital[13].write(0)
+#     time.sleep(1)
+
+# board.digital[10].mode = pyfirmata.INPUT
+# sw = board.digital[10].read()
+# if sw:
+#     board.digital[13].write(1)
+# else:
+#     board.digital[13].write(0)
